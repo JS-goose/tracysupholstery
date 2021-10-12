05955eee9d9e1608319e56bd22872798992ab0ab
Jonathan Sexton <joncsexton@gmail.com>
2021-10-12 05:00:14 -0500

    config update

--- v2/gatsby-config.js
+++ v2/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby TailwindCSS Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter with TailwindCSS and common dev tools. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@kosvrouvas`,
+    title: `Tracy's Upholstery`,
+    description: `An upholstery shop specializing in custom and repair work for marine, ATV, and motorcycles`,
+    author: `@JS-goose`,
     url: `https://tracys-upholstery.com`,
     image: `src/images/gatsby-icon.png`
   },
@@ -11,6 +11,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layout.js`),
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
