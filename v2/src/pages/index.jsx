d6fc3a8cb10deeb4759af9a4f0b584812b5317a8
Jonathan Sexton <joncsexton@gmail.com>
2021-10-22 21:49:41 -0500

    added some hero images

--- v2/src/pages/index.jsx
+++ v2/src/pages/index.jsx
@@ -5,7 +5,7 @@ import SeO from "../components/seo"
 const IndexPage = () => (
   <div className="index-wrapper flex flex-col items-center">
     <SeO title="Home" />
-    <div>hello from index</div>
+    <div>Howdy from index</div>
   </div>
 )
 
